f9b9eed73d237c5223821cdf27b24b50f7d75e90
KyleHarsh <duhboss1010900@gmail.com>
2024-09-20 16:59:06 -0400

    Began outlining high level structure

---
+++ src/graph.h
@@ -0,0 +1,17 @@
+#ifndef GRAPH_H
+
+#include "node.h"
+
+/*
+ * This class is represented as a tree of nodes.
+ * Each level of the tree is seperated by one cut from the previous level.
+*/
+class graph
+{
+private:
+    node root;
+    /* data */
+public:
+};
+
+#endif
---
+++ src/node.h
@@ -0,0 +1,14 @@
+#include <vector>
+#include <string>
+
+/*
+ * Each node within the graph represents all contents of a cut
+*/
+class node
+{
+private:
+    std::vector<node> children;
+    std::vector<std::string> atoms;
+public:
+
+};
